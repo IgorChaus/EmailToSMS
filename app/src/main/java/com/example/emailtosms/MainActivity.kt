098b59e95ec187bf2e1420d9a77b95c97281baeb
Igor Chaus <igor_chaus@mail.ru>
2023-06-03 09:15:18 +0200

    Refactored code using checkEmail

--- app/src/main/java/com/example/emailtosms/MainActivity.kt
+++ app/src/main/java/com/example/emailtosms/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.emailtosms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.emailtosms.domain.EmailListRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val emailListRepositoryImpl = EmailListRepositoryImpl()
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val emailListRepositoryImpl = EmailListRepositoryImpl()
+            emailListRepositoryImpl.checkEmail(
+                "alarm-parking@mail.ru",
+                "rQQz8Lq5zcTSdkPpb6W7",
+                "imap.mail.ru",
+                "995"
+            )
+        }
     }
 }
--- app/src/main/java/com/example/emailtosms/domain/EmailListRepositoryImpl.kt
+++ app/src/main/java/com/example/emailtosms/domain/EmailListRepositoryImpl.kt
@@ -7,69 +7,105 @@ import java.util.*
 import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.search.FlagTerm
+import kotlin.concurrent.thread
 
 class EmailListRepositoryImpl {
 
+    fun openStore(user: String, password: String, host: String, port: String): Store{
+        val properties = Properties()
+        properties["mail.imap.host"] = host
+        properties["mail.imap.port"] = port
+        properties["mail.imap.starttls.enable"] = "true"
+
+        val store = Session.getDefaultInstance(properties).getStore("imaps")
+        store.connect(host, user, password)
+
+        return store
+    }
+
+    fun openFolder(store: Store): Folder{
+        val emailFolder = store.getFolder("INBOX")
+        emailFolder.open(Folder.READ_WRITE)
+        return emailFolder
+    }
+
+    fun getMessageList(emailFolder: Folder): Array<Message>{
+        val ft = FlagTerm(Flags(Flags.Flag.SEEN), false)
+        val messageList = emailFolder.search(ft)
+        Log.i("MyTag", "Сообщений в ящике:" + messageList.size)
+        return messageList
+    }
+
+    fun deleteMessage(message: Message) =
+        message.setFlag(Flags.Flag.DELETED, true)
+
+
+
+
     fun checkEmail(user: String, password: String, host: String, port: String) {
+        val emailList = arrayListOf<EmailItem>()
 
         val properties = Properties()
         properties["mail.imap.host"] = host
         properties["mail.imap.port"] = port
         properties["mail.imap.starttls.enable"] = "true"
 
-        try {
+        thread {
+            try {
+                Log.i("MyTag", "T1")
+                val store = Session.getDefaultInstance(properties).getStore("imaps")
+                Log.i("MyTag", "T2")
+                store.connect(host, user, password)
+                val emailFolder = store.getFolder("INBOX")
+                emailFolder.open(Folder.READ_WRITE)
 
-            val emailSession = Session.getDefaultInstance(properties)
-            val store = emailSession.getStore("imaps")
-            store.connect(host, user, password)
-            val emailFolder = store.getFolder("INBOX")
-            emailFolder.open(Folder.READ_WRITE)
+                val ft = FlagTerm(Flags(Flags.Flag.SEEN), false)
+                val messageList = emailFolder.search(ft)
+                Log.i("MyTag", "Сообщений в ящике:" + messageList.size)
 
-            val ft = FlagTerm(Flags(Flags.Flag.SEEN), false)
-            val messages = emailFolder.search(ft)
-            Log.i("MyTag", "Сообщений в ящике:" + messages.size)
-
-            val getMulti = GetMulti()
-            var count = 1
-            while (count <= messages.size) {
-                val message = messages[count]
-                val subject = message.getSubject().trim()
-                val froms = message.getFrom()
-                val email: String? = (froms[0] as InternetAddress).address
-                val text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    Html.fromHtml(getMulti.getText(message), Html.FROM_HTML_MODE_LEGACY).toString()
-                } else {
-                    Html.fromHtml(getMulti.getText(message)).toString()
-                }
-                message.setFlag(Flags.Flag.DELETED, true)
-
-                if (subject.equals("5791")) {
-                    val text = text.trim()
-                    val space = text.indexOf(" ")
-                    var phone = text.substring(0, space)
-
-                    val re = Regex("[^0-9]")
-                    if (phone[0] == '+') {
-                        phone = re.replace(phone, "")
-                        phone = "+ $phone"
+                val getMulti = GetMulti()
+                var count = 1
+                while (count <= messageList.size) {
+                    val message = messageList[count]
+                    val emailSubject = message.getSubject().trim()
+                    var emailMessage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        Html.fromHtml(getMulti.getText(message), Html.FROM_HTML_MODE_LEGACY)
+                            .toString()
                     } else {
-                        phone = re.replace(phone, "")
+                        Html.fromHtml(getMulti.getText(message)).toString()
                     }
+                    message.setFlag(Flags.Flag.DELETED, true)
 
-                    var messageText = text.substring(space).trim()
-                    messageText = messageText.replace("\n", "")
+                    if (emailSubject.equals("5791")) {
+                        emailMessage = emailMessage.trim()
+                        val spaceIndex = emailMessage.indexOf(" ")
+                        var phone = emailMessage.substring(0, spaceIndex)
+
+                        val re = Regex("[^0-9]")
+                        if (phone[0] == '+') {
+                            phone = re.replace(phone, "")
+                            phone = "+ $phone"
+                        } else {
+                            phone = re.replace(phone, "")
+                        }
+
+                        var smsMessage = emailMessage.substring(spaceIndex).trim()
+                        smsMessage = smsMessage.replace("\n", "")
+                        emailList.add(EmailItem(phone, smsMessage))
+                    }
+                    count++
                 }
-                count++
-            }
-            emailFolder.close(true)
-            store.close()
+                emailFolder.close(true)
+                store.close()
 
-        }catch(e: NoSuchProviderException){
-            Log.i("MyTag", "Error in NoSuchProviderException. +$e")
-        }catch(e: MessagingException) {
-            Log.i("MyTag", "Error in MessagingException. +$e")
-        }catch (e: Exception){
-            Log.i("MyTag", "Error in EmailListRepositoryImpl. +$e")
+            } catch (e: NoSuchProviderException) {
+                Log.i("MyTag", "NoSuchProviderException $e")
+            } catch (e: MessagingException) {
+                Log.i("MyTag", "MessagingException $e")
+            } catch (e: Exception) {
+                Log.i("MyTag", "Exception in EmailListRepositoryImpl $e")
+            }
         }
     }
+
 }
