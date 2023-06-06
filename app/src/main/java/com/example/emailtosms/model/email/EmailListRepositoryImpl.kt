dcd957d0c3ca758a16b9fab8127e3f35ca997032
Igor Chaus <igor_chaus@mail.ru>
2023-06-06 17:37:31 +0200

    Added synchronized in getEmailList

--- app/src/main/java/com/example/emailtosms/model/email/EmailListRepositoryImpl.kt
+++ app/src/main/java/com/example/emailtosms/model/email/EmailListRepositoryImpl.kt
@@ -4,15 +4,12 @@ import android.os.Build
 import android.text.Html
 import android.util.Log
 import com.example.emailtosms.domain.email.EmailItem
-import com.sun.mail.iap.Response
 import java.util.*
 import javax.mail.*
 import javax.mail.search.FlagTerm
-import kotlin.concurrent.thread
 
 class EmailListRepositoryImpl {
 
-    @Synchronized
     fun getEmailList(
         user: String,
         password: String,
@@ -21,47 +18,49 @@ class EmailListRepositoryImpl {
         isDeleted: Boolean
     ):EmailResponse{
 
-        val emailList = arrayListOf<EmailItem>()
-        var response: String? = OK
+        synchronized(LOCK){
+            val emailList = arrayListOf<EmailItem>()
+            var response: String? = OK
 
-        val properties = Properties()
-        properties["mail.imap.host"] = host
-        properties["mail.imap.port"] = port
-        properties["mail.imap.starttls.enable"] = "true"
-        try {
-            val store = Session.getDefaultInstance(properties).getStore("imaps")
-            store.connect(host, user, password)
-            val folder = store.getFolder("INBOX")
-            folder.open(Folder.READ_WRITE)
+            val properties = Properties()
+            properties["mail.imap.host"] = host
+            properties["mail.imap.port"] = port
+            properties["mail.imap.starttls.enable"] = "true"
+            try {
+                val store = Session.getDefaultInstance(properties).getStore("imaps")
+                store.connect(host, user, password)
+                val folder = store.getFolder("INBOX")
+                folder.open(Folder.READ_WRITE)
 
-            val ft = FlagTerm(Flags(Flags.Flag.SEEN), false)
-            val messageList = folder.search(ft)
-            Log.i("MyTag", "Сообщений в ящике:" + messageList.size)
+                val ft = FlagTerm(Flags(Flags.Flag.SEEN), false)
+                val messageList = folder.search(ft)
+                Log.i("MyTag", "Сообщений в ящике:" + messageList.size)
 
-            var count = 0
-            while (count < messageList.size) {
-                val message = messageList[count]
-                if (message.subject.trim() == "5791") {
-                    val item = mapEmailMessageToEmailItem(message)
-                    emailList.add(item)
+                var count = 0
+                while (count < messageList.size) {
+                    val message = messageList[count]
+                    if (message.subject.trim() == "5791") {
+                        val item = mapEmailMessageToEmailItem(message)
+                        emailList.add(item)
+                    }
+                    if (isDeleted) {
+                        message.setFlag(Flags.Flag.DELETED, true)
+                    }
+                    count++
                 }
-                if (isDeleted) {
-                    message.setFlag(Flags.Flag.DELETED, true)
-                }
-                count++
+                folder.close(true)
+                store.close()
+            } catch (e: AuthenticationFailedException){
+                response = "AuthenticationFailedException $e"
+            } catch (e: NoSuchProviderException) {
+                response = "NoSuchProviderException $e"
+            } catch (e: MessagingException) {
+                response = "MessagingException $e"
+            } catch (e: Exception) {
+                response = "Exception in EmailListRepositoryImpl $e"
             }
-            folder.close(true)
-            store.close()
-        } catch (e: AuthenticationFailedException){
-            response = "AuthenticationFailedException $e"
-        } catch (e: NoSuchProviderException) {
-            response = "NoSuchProviderException $e"
-        } catch (e: MessagingException) {
-            response = "MessagingException $e"
-        } catch (e: Exception) {
-            response = "Exception in EmailListRepositoryImpl $e"
+            return EmailResponse(emailList, response)
         }
-        return EmailResponse(emailList, response)
     }
 
     private fun mapEmailMessageToEmailItem(message: Message): EmailItem {
@@ -70,6 +69,7 @@ class EmailListRepositoryImpl {
                 Html.fromHtml(getMulti.getTextFromMessage(message), Html.FROM_HTML_MODE_LEGACY)
                     .toString()
             } else {
+                @Suppress("DEPRECATION")
                 Html.fromHtml(getMulti.getTextFromMessage(message)).toString()
             }
             emailMessage = emailMessage.trim()
@@ -89,8 +89,8 @@ class EmailListRepositoryImpl {
             return EmailItem(phone, smsMessage)
     }
 
-    companion object{
+    companion object {
         val OK = null
+        private var LOCK = Any()
     }
-
 }
