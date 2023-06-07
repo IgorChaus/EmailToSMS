62b825744a6500b04f405b93ff307348d41956f7
Igor Chaus <igor_chaus@mail.ru>
2023-06-07 19:14:43 +0200

    Replaced Option Menu to Bottom Menu

--- app/src/main/java/com/example/emailtosms/presentation/ListFragment.kt
+++ app/src/main/java/com/example/emailtosms/presentation/ListFragment.kt
@@ -1,11 +1,9 @@
 package com.example.emailtosms.presentation
 
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import com.example.emailtosms.R
 import com.example.emailtosms.databinding.ListScreenBinding
 
 class ListFragment: Fragment() {
@@ -18,7 +16,7 @@ class ListFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = ListScreenBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,6 +24,7 @@ class ListFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
