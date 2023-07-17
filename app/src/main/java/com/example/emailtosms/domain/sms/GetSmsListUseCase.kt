f6f4b5bf0ad92c79307ab076bdbffc3f8a5ea458
Igor Chaus <igor_chaus@mail.ru>
2023-07-17 19:49:20 +0200

    Maked changing length of log

--- app/src/main/java/com/example/emailtosms/domain/sms/GetSmsListUseCase.kt
+++ app/src/main/java/com/example/emailtosms/domain/sms/GetSmsListUseCase.kt
@@ -3,5 +3,5 @@ package com.example.emailtosms.domain.sms
 import androidx.lifecycle.LiveData
 
 class GetSmsListUseCase(private val smsListRepository: SmsListRepository) {
-    fun getSmsList(): LiveData<List<SmsItem>> = smsListRepository.getSmsList()
+    fun getSmsList(limit: Int): LiveData<List<SmsItem>> = smsListRepository.getSmsList(limit)
 }
