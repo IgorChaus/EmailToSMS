91c2ee597621ac5470607265e41f5e0c03b0b39c
Igor Chaus <igor_chaus@mail.ru>
2023-08-09 21:11:58 +0200

    Up library versions and set dependences for dagger

--- app/src/main/java/com/example/emailtosms/data/database/SmsListRepositoryImpl.kt
+++ app/src/main/java/com/example/emailtosms/data/database/SmsListRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.emailtosms.data.database
 
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.map
 import com.example.emailtosms.data.mapper.MapperSmsItemToEntity
 import com.example.emailtosms.domain.sms.SmsItem
 import com.example.emailtosms.domain.sms.SmsListRepository
@@ -11,11 +11,10 @@ class SmsListRepositoryImpl(context: Context): SmsListRepository {
     private val smsListDao = AppDataBase.getInstance(context).smsListDao()
     private val mapper = MapperSmsItemToEntity()
 
-    override fun getSmsList(limit: Int): LiveData<List<SmsItem>> = Transformations.map(
-        smsListDao.getSmsList(limit)){
-        mapper.mapListDbModelToListEntity(it)
-    }
-
+    override fun getSmsList(limit: Int): LiveData<List<SmsItem>> =
+        smsListDao.getSmsList(limit).map {
+            mapper.mapListDbModelToListEntity(it)
+        }
 
     override suspend fun addSmsItem(smsItem: SmsItem) {
         smsListDao.addSmsItem(mapper.mapEntityToDbModel(smsItem))
